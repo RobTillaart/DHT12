764304324d59fa319fa99acfb7ac86f09a043b47
rob tillaart <rob.tillaart@gmail.com>
2021-12-16 14:01:37 +0100

    update library.json, license, add isConnecter()

--- DHT12.h
+++ DHT12.h
@@ -3,7 +3,7 @@
 //    FILE: DHT12.h
 //  AUTHOR: Rob Tillaart
 // PURPOSE: DHT_I2C library for Arduino .
-// VERSION: 0.3.1
+// VERSION: 0.3.2
 // HISTORY: See DHT12.cpp
 //     URL: https://github.com/RobTillaart/DHT12
 //
@@ -12,12 +12,12 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-#define DHT12_LIB_VERSION      "0.3.1"
+#define DHT12_LIB_VERSION                    (F("0.3.2"))
 
-#define DHT12_OK               0
-#define DHT12_ERROR_CHECKSUM  -10
-#define DHT12_ERROR_CONNECT   -11
-#define DHT12_MISSING_BYTES   -12
+#define DHT12_OK                             0
+#define DHT12_ERROR_CHECKSUM                -10
+#define DHT12_ERROR_CONNECT                 -11
+#define DHT12_MISSING_BYTES                 -12
 
 
 class DHT12
@@ -29,6 +29,8 @@ public:
   void     begin(const uint8_t dataPin, const uint8_t clockPin);
 #endif
   void     begin();
+
+  bool     isConnected();
 
   int8_t   read();
   float    getHumidity()    { return _humidity    + _humOffset; };
